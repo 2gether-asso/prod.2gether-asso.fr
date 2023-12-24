46acc01a01a76e7d1926c74c44b48801330a1305
Matiboux <matiboux@gmail.com>
2023-12-24 06:33:00 +0100

    Add description

--- app/app/src/i18n/locales/fr.ts
+++ app/app/src/i18n/locales/fr.ts
@@ -6,6 +6,10 @@ const locale =
 	'Subscribe on YouTube': 'Abonne-toi sur YouTube',
 	'Visit our website': 'Visite notre site web',
 	'Join our Discord': 'Rejoins notre Discord',
+	'2GETHER wishes to offer you more worked content and therefore sets up a new YouTube channel: 2GETHER PROD!':
+		'2GETHER souhaite vous proposer des contenus plus travaillés et met donc en place une nouvelle chaine YouTube : 2GETHER PROD !',
+	'On the agenda: popularization videos, short films, and many other surprises!':
+		'Au programme : des vidéos de vulgarisations, des courts métrages, et pleins d\'autres surprises !',
 } as const
 
 // Static type check
